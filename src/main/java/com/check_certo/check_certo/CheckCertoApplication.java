fdfeede2abb544fadc548532b5375e0e1f45e6dc
lps271 <lpslucas27@gmail.com>
2024-10-16 06:15:30 -0300

    Criacão do projeto Spring

---
+++ src/main/java/com/check_certo/check_certo/CheckCertoApplication.java
@@ -0,0 +1,13 @@
+package com.check_certo.check_certo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckCertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/check_certo/check_certo/CheckCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.check_certo.check_certo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
